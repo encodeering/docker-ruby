fbcac85ce05899213981d71cc206ddceaf24208c
Michael Clausen <encodeering@gmail.com>
2017-11-29 13:00:20 +0100

    removes onbuild split
    * armhf will now be completely build by shippable, so there's no need for a splitted build anymore

--- README.md
+++ README.md
@@ -6,16 +6,10 @@
 
 ```docker pull encodeering/ruby-armhf```
 
-- 2.3, [2.3-onbuild]
+- 2.3
 - https://hub.docker.com/r/encodeering/ruby-armhf/
 
 ```docker pull encodeering/ruby-amd64```
 
-- 2.3, [2.3-onbuild]
+- 2.3
 - https://hub.docker.com/r/encodeering/ruby-amd64/
-
-### Modification
-
-The build time for a armhf image was greater than the allowed maximum of 50 min and we had to split the official ruby build into two parts therefore.
-
-- 2.3-onbuild includes the compile and install steps. Please look at the [patch](.patch/2.3/Dockerfile.patch) for further information.
--- modules/build.sh
+++ modules/build.sh
@@ -11,15 +11,8 @@ set -e
 import com.encodeering.docker.config
 import com.encodeering.docker.docker
 
-if [ "$ONBUILD" = true ]; then
-    docker-pull "$REPOSITORY/buildpack-$ARCH:jessie" "buildpack-deps:jessie"
+docker-pull "$REPOSITORY/buildpack-$ARCH:jessie" "buildpack-deps:jessie"
 
-    patch -p1 --no-backup-if-mismatch --directory=$PROJECT < patch/$VERSION/Dockerfile.patch
+docker build -t "$DOCKER_IMAGE" "$PROJECT/$VERSION"
 
-    docker build -t "$DOCKER_IMAGE-onbuild" "$PROJECT/$VERSION"
-else
-    echo       "FROM $DOCKER_IMAGE-onbuild" > Dockerfile.onbuild
-    docker build -t "$DOCKER_IMAGE" -f Dockerfile.onbuild .
-
-    docker run --rm "$DOCKER_IMAGE" ruby --version
-fi
+docker run --rm "$DOCKER_IMAGE" ruby --version
