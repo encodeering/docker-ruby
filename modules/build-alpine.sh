3c7f88e243c35d50bd712189ae2eb4ad361540e0
Michael Clausen <encodeering@gmail.com>
2018-01-06 18:58:39 +0100

    adds an alpine image for ruby 2.5
    * latest release so far

--- modules/build-alpine.sh
+++ modules/build-alpine.sh
@@ -11,6 +11,6 @@ set -e
 import com.encodeering.docker.config
 import com.encodeering.docker.docker
 
-./build-${BASE}.sh
+docker-pull "$REPOSITORY/alpine-$ARCH:3.7" "alpine:3.7"
 
-docker run --rm "$DOCKER_IMAGE" ruby --version
+docker build -t "$DOCKER_IMAGE" "$PROJECT/$VERSION/alpine3.7"
---
+++ modules/build-debian.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# configuration
+#   env.ARCH
+#   env.PROJECT
+#   env.VERSION
+#   env.VARIANT
+#   env.REPOSITORY
+
+set -e
+
+import com.encodeering.docker.config
+import com.encodeering.docker.docker
+
+case "$VERSION" in
+    2.3) FROM=jessie;   TO=jessie  ;;
+    2.5) FROM=stretch ; TO=stretch ;;
+esac
+
+docker-pull "$REPOSITORY/buildpack-$ARCH:$FROM" "buildpack-deps:$TO"
+
+docker build -t "$DOCKER_IMAGE" "$PROJECT/$VERSION/$FROM"
