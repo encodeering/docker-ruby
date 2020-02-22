6bd3a347328a4646ca665fd8b49b1b22aefdf462
Michael Clausen <encodeering@gmail.com>
2020-02-22 16:11:42 +0100

    updating scm repository to latest revision
    * e30082fa0ddb6ee6c03ce2fac005725a0612c2cd
    
    NOTE
    
    * requires an update of the base images for alpine

--- modules/build-debian.sh
+++ modules/build-debian.sh
@@ -5,6 +5,6 @@ set -e
 import com.encodeering.ci.config
 import com.encodeering.ci.docker
 
-docker-pull "$REPOSITORY/buildpack-$ARCH:stretch" "buildpack-deps:stretch"
+docker-pull "$REPOSITORY/buildpack-$ARCH:buster" "buildpack-deps:buster"
 
-docker-build "$PROJECT/$VERSION/stretch"
+docker-build "$PROJECT/$VERSION/buster"
